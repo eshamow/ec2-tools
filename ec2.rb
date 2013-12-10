6eddc40ec6ca3c544a7ffb75cba3fb15e44ae4aa
Eric Shamow <eric.shamow@gmail.com>
2013-12-10 10:46:55 -0800

    Moved more functionality into ec2

--- ec2.rb
+++ ec2.rb
@@ -10,7 +10,8 @@ def LoadConfig(path)
   AWS.ec2.client
 end
 
-def validate(options,env)
+def validate(options,env,command)
+  vals = Hash.new()
   if options[:keyset]
     keyset = options[:keyset]
   elsif env['AWS_KEYSET']
@@ -24,10 +25,19 @@ def validate(options,env)
     configfile = '~/.awsconfig'
   end
 
-  {
-    :keyset => keyset,
-    :configfile => configfile
-  }
+  if command == 'listinstances'
+    if options[:state]
+      state = options[:state]
+    else
+      state = 16
+    end
+    vals[:state] = Integer(state)
+  end
+
+  vals[:keyset] = keyset
+  vals[:configfile] = configfile
+
+  vals
 end
 
 class EC2 < Thor
@@ -37,18 +47,12 @@ class EC2 < Thor
   desc "listinstances", "List running EC2 instances"
   option :state
   def listinstances
-    if options[:state]
-      state = options[:state].to_i
-    else
-      state = 16
-    end
-
-    opts = validate options, ENV
+    opts = validate options, ENV, 'listinstances'
     client = LoadConfig opts[:configfile]
     resp = client.describe_instances(filters: [{ name: 'key-name', values: [opts[:keyset]] }])
     resp[:reservation_set].each { |reservation|
       reservation[:instances_set].each { |instance|
-        if instance[:instance_state][:code] == state
+        if instance[:instance_state][:code] == opts[:state]
           name = instance[:dns_name]
           ip = instance[:ip_address]
           id = instance[:instance_id]
@@ -62,13 +66,41 @@ class EC2 < Thor
   desc "deleteinstances", "delete EC2 instances"
   option :id
   def deleteinstances
-    opts = validate options, ENV
+    opts = validate options, ENV, 'deleteinstances'
     client = LoadConfig opts[:configfile]
-    validate options, ENV
     begin
       resp = client.terminate_instances({ :instance_ids => [options[:id]] })
     rescue => e
       puts e
+    end
+  end
+
+  desc "runinstances", "run EC2 instances"
+  option :ami
+  option :count
+  option :groups
+  option :keyset
+  option :groups
+  option :type
+  option :ebs
+  def runinstances
+    opts = validate options, ENV, 'runinstances'
+    client = LoadConfig opts[:configfile]
+    resp = client.run_instances({
+      :image_id => options[:image_id],
+      :min_count => options[:count],
+      :max_count => options[:count],
+      :key_name => options[:keyset],
+      :security_groups => options[:groups],
+      :instance_type => options[:type],
+      :ebs_optimized => options[:ebs],
+    })
+    if resp[:instance_state][:code] == state
+      name = resp[:dns_name]
+      ip = resp[:ip_address]
+      id = resp[:instance_id]
+      int_dns = resp[:private_dns_name]
+      puts "#{name} #{ip} #{id} #{int_dns}"
     end
   end
 end
